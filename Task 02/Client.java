a02d9e4f4a3a0457a4ca788609f1665474f03130
Gladwin Solomon <gladwinsolomon006@gmail.com>
2026-08-18 14:06:35 +0530

    Add files via upload

---
+++ Task 02/Client.java
@@ -0,0 +1,57 @@
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Client {
+
+    @Id
+    @GeneratedValue
+    private long clientId;
+
+    @ManyToOne
+    private Advisor advisor;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    protected Client() {
+    }
+
+    public Client(Advisor advisor, String name, String email) {
+        this.advisor = advisor;
+        this.name = name;
+        this.email = email;
+    }
+
+    public long getClientId() {
+        return clientId;
+    }
+
+    public Advisor getAdvisor() {
+        return advisor;
+    }
+
+    public void setAdvisor(Advisor advisor) {
+        this.advisor = advisor;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
